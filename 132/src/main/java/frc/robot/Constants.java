830321b54fcc83448eb92c0aa5a59577eb8ad0ae
Ray <tanxi3003@gmail.com>
2025-02-14 10:46:25 -0500

    Init commit

--- 132/src/main/java/frc/robot/Constants.java
+++ 132/src/main/java/frc/robot/Constants.java
@@ -15,5 +15,7 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    // isabel is the best
+    int monkey =0;
   }
 }
